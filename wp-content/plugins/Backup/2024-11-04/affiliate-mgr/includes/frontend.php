b162e72f32dcb08f210844a4e23fb588e5bf2923
Jonathan Borthwick <jborthwickog@gmail.com>
2024-11-13 21:39:34 -0800

    Remove raw backups
    Remove raw backup content to prep for zipped version

--- wp-content/plugins/Backup/2024-11-04/affiliate-mgr/includes/frontend.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once plugin_dir_path(__FILE__) . 'redirect-handler.php';
-function affiliate_mgr_handler() {
-    affiliate_mgr_redirect();
-}
-add_action('template_redirect', 'affiliate_mgr_handler');
